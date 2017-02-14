1592f2b77c786dcbff4dfd2b2ed212286eae0e46
Taguhi Abgaryan <taguhiabgaryan@taguhis-macbook-pro.local>
2017-02-14 17:01:08 +0400

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Taguhi Abgaryan on 2/14/17.
+//  Copyright © 2017 Taguhi Abgaryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
