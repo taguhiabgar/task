5112a3cfe7a6dc20c56b8750baf831e1ab25d370
Taguhi Abgaryan <taguhiabgaryan@taguhis-macbook-pro.local>
2017-02-15 13:21:20 +0400

    Doing JSON request and converting data into Swift array. DataManager class added. When choosing a tableView cell, Detailed VC is shown.

---
+++ Task/DataManager.swift
@@ -0,0 +1,44 @@
+//
+//  Data.swift
+//  Task
+//
+//  Created by Taguhi Abgaryan on 2/14/17.
+//  Copyright © 2017 Taguhi Abgaryan. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class DataManager {
+    
+    // MARK: - Static Properties
+    
+    static let shared = DataManager()
+    
+    // MARK: - Public Properties
+    
+    public var data: Array<Dictionary<String, Any>> = []
+    
+    // MARK: - Public Methods
+    
+    public func requestData(url: String) {
+        var jsonArray: Array<Dictionary<String, Any>> = []
+        Alamofire.request(url).responseJSON { (responseObject) -> Void in
+            jsonArray = self.convertToDictionary(response: responseObject)
+            } .responseJSON(completionHandler: { _ in
+                self.data = jsonArray
+            }
+        )
+    }
+    
+    public func remove(index: Int) {
+        print("not implemented: remove(index: Int)")
+    }
+    
+    // MARK: - Private Methods
+    
+    private func convertToDictionary(response: DataResponse<Any>) -> Array<Dictionary<String, Any>> {
+        return response.result.value as! Array<Dictionary<String, Any>>
+    }
+    
+}
--- Task/ViewController.swift
+++ Task/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
+let urlString = "https://jsonplaceholder.typicode.com/photos?albumId[]=95&albumId[]=1"
+
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var tableView = UITableView()
+    
+    var tableViewData = ["item 1", "item 2", "item 3"]
+    
+    let cellReuseIdentifier = "cell"
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        view.backgroundColor = UIColor.gray
+        requestData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func requestData() {
+        DataManager.shared.requestData(url: urlString)
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.frame = self.view.frame
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        self.view.addSubview(tableView)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableViewData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)! as UITableViewCell
+        cell.textLabel?.text = self.tableViewData[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("--> #\(indexPath.row)")
+        let vc = DetailedViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
